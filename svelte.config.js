7554811f48ea2810581886ae8088145998e153ec
Alex <pinette_alex@wheatoncollege.edu>
2026-01-03 22:04:55 -0500

    prep for prod deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,9 +12,8 @@ const config = {
 		inspector: true,
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// Using Vercel adapter for deployment
+		// See https://kit.svelte.dev/docs/adapter-vercel for more information
 		adapter: adapter(),
 		env: { dir: './' },
 	},
